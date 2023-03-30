167e5a3bcea666997d7899c31d5563b4fd547a79
beardedsakimonkey <sakimonkey@protonmail.com>
2023-03-30 09:30:42 -0700

    add menu prices

--- menu.lua
+++ menu.lua
@@ -10,8 +10,7 @@ function Menu.new(cfg)
         w=cfg.w, h=cfg.h,
         is_open=false,
         items={},
-        sel_item=1,
-        items_y=cfg.items_y,
+        cur_idx=1,
     }
     setmetatable(m, {__index = Menu})
     return m
@@ -27,23 +26,23 @@ function Menu:draw()
     end
 
     for i, item in ipairs(self.items) do
-        local color = (i == self.sel_item) and C.white or C.light_gray
-        print(item.text, self.x+9, self.y+self.items_y+8*(i-1), color)
+        local color = (i == self.cur_idx) and C.white or C.light_gray
+        print(item.text, self.x+9, self.y+item.y, color)
     end
 
-    print('>', self.x+3, self.y+self.items_y+8*(self.sel_item-1), C.white)
+    print('>', self.x+3, self.y+self.items[self.cur_idx].y, C.white)
 end
 
 function Menu:update()
     if not self.is_open then return end
     if btnp(B.up) then
-        self.sel_item = wrap(1, self.sel_item-1, #self.items)
+        self.cur_idx = wrap(1, self.cur_idx-1, #self.items)
     end
     if btnp(B.down) then
-        self.sel_item = wrap(1, self.sel_item+1, #self.items)
+        self.cur_idx = wrap(1, self.cur_idx+1, #self.items)
     end
     if btnp(B.z) then
-        self.items[self.sel_item].cb(self)
+        self.items[self.cur_idx].cb(self)
         self:close()
     end
     if btnp(B.x) then
@@ -56,15 +55,15 @@ function Menu:close()
 end
 
 function Menu:open()
-    self.sel_item = 1
+    self.cur_idx = 1
     self.is_open = true
 end
 --------------------------------------------------------------------------------
 
 function init_menus()
-    buy_menu = Menu.new({x=35, y=97, w=59, h=27, items_y=11})
-    add(buy_menu.items, {text='buy',    cb=do_buy})
-    add(buy_menu.items, {text='cancel', cb=buy_menu.close})
+    buy_menu = Menu.new({x=35, y=97, w=59, h=27})
+    add(buy_menu.items, {text='buy',    y=11+8*0, cb=do_buy})
+    add(buy_menu.items, {text='cancel', y=11+8*1, cb=buy_menu.close})
 
     buy_menu.sel_twr = 1
 
@@ -99,16 +98,24 @@ function init_menus()
             print('➡️', self.x+38, self.y+3, C.dark_gray)
         end
         spr(self.sel_twr, self.x+26, self.y+2)
+        print('20', self.x+26, self.y+11+8*0, C.indigo)
     end
 
-    upg_menu = Menu.new({x=35, y=89, w=59, h=36, items_y=12})
-    add(upg_menu.items, {text='upgrade', cb=do_upgrade})
-    add(upg_menu.items, {text='sell',    cb=do_sell})
-    add(upg_menu.items, {text='cancel',  cb=upg_menu.close})
+    buy_menu.open = function(self)
+        Menu.open(self)
+        self.sel_twr = 1
+    end
+
+    upg_menu = Menu.new({x=35, y=89, w=59, h=36})
+    add(upg_menu.items, {text='upgrade', y=12+8*0, cb=do_upgrade})
+    add(upg_menu.items, {text='sell',    y=12+8*1, cb=do_sell})
+    add(upg_menu.items, {text='cancel',  y=12+8*2, cb=upg_menu.close})
 
     upg_menu.draw = function(self)
         if not self.is_open then return end
         Menu.draw(self)
+        print('20', self.x+44, self.y+12+8*0, C.indigo)
+        print('20', self.x+44, self.y+12+8*1, C.indigo)
         spr(self.twr, self.x+26, self.y+2)
     end
 
