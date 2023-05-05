fc0c8d6cce8efa1a82d604f0665fc79ec1fdb003
beardedsakimonkey <sakimonkey@protonmail.com>
2023-05-04 21:18:54 -0700

    add glisten to hint button
    wrapped includes in do..end so that locals in one file don't contaminate the
    scope of another file

--- hint.lua
+++ hint.lua
@@ -3,16 +3,22 @@ show_hint = true
 local prev_show_hint = show_hint
 
 local OFFSCREEN_Y = -6
-local hint_y = OFFSCREEN_Y
-local hint_v = 0
+local y = OFFSCREEN_Y
+local v = 0
+local t = 0
 
 function update_hint()
     show_hint = show_hint or can_send_wave()
-    if show_hint ~= prev_show_hint then
-        hint_v = 200
+    if show_hint then
+        t += 1
+    else
+        t = 0
+    end
+    if prev_show_hint and not show_hint then -- dismissing hint
+        v = 200
     end
     local dest_y = show_hint and 2 or OFFSCREEN_Y
-    hint_y, hint_v = spring(hint_y, dest_y, hint_v, {
+    y, v = spring(y, dest_y, v, {
         stiffness = 200,
         damping = 32,
         mass = 2,
@@ -23,9 +29,11 @@ end
 
 function draw_hint()
     local x = 27
-    spr(20, x + (cos(time()/2)+2)*1.2 - 2, hint_y)
+    -- Draw arrow
+    spr(20, x, y)
     x += 6
-    print('❎', x, hint_y+1, C.indigo)
-    rectfill(x+2, hint_y+1, x+4, hint_y+3, C.black)
-    print('❎', x, hint_y, C.light_gray)
+    -- Draw button
+    local glisten_delay = 300 -- 10 sec
+    local s = 48 + max(0, (t\2)%(glisten_delay+11) - glisten_delay)
+    spr(s, x, y)
 end
