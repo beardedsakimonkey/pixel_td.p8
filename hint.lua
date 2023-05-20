4412ba0e7d4c5620d0f299b7e3402bcab022d68a
beardedsakimonkey <sakimonkey@protonmail.com>
2023-05-20 13:54:54 -0700

    add border around button sprite

--- hint.lua
+++ hint.lua
@@ -5,7 +5,7 @@ show_hint = true
 
 local prev_show_hint = show_hint
 
-local OFFSCREEN_Y = -6
+local OFFSCREEN_Y = -8
 local y = OFFSCREEN_Y
 local v = 0
 local t = 0
@@ -20,7 +20,7 @@ function update_hint()
     if prev_show_hint and not show_hint then -- dismissing hint
         v = 200
     end
-    local dest_y = show_hint and 2 or OFFSCREEN_Y
+    local dest_y = show_hint and 1 or OFFSCREEN_Y
     y, v = spring(y, dest_y, v, {
         stiffness = 200,
         damping = 32,
@@ -31,20 +31,21 @@ function update_hint()
 end
 
 function draw_hint()
-    local x = 27
+    local x = 26
     -- Draw arrow
     pal(C.green, C.black)
-    spr(20, x-1, y-1)
-    pal(0)
+    spr(20, x, y)
     x += 6
     -- Draw button
     local glisten_delay = 300 -- 10 sec
-    local s = 48 + max(0, (t\2)%(glisten_delay+11) - glisten_delay)
-    spr(s, x, y)
+    local s = max(0, (t\2)%(glisten_delay+11) - glisten_delay)
+    sspr(s*9, 24, 9, 8, x, y)
+
     -- draw 'x'
-    pset(x+2, y+1, C.dark_blue)
-    pset(x+4, y+1, C.dark_blue)
-    pset(x+2, y+3, C.dark_blue)
-    pset(x+4, y+3, C.dark_blue)
     pset(x+3, y+2, C.dark_blue)
+    pset(x+5, y+2, C.dark_blue)
+    pset(x+3, y+4, C.dark_blue)
+    pset(x+5, y+4, C.dark_blue)
+    pset(x+4, y+3, C.dark_blue)
+    pal(0)
 end
