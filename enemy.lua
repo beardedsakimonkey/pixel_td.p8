22432c87a66c97c161c67aaaf0034cafaa641379
beardedsakimonkey <sakimonkey@protonmail.com>
2023-08-06 21:15:27 -0700

    tuning

--- enemy.lua
+++ enemy.lua
@@ -33,7 +33,7 @@ end
 function spawn_enemy()
     if sending > 0 then
         local type = ({'CIRCLE', 'SQUARE', 'DIAMOND', 'RECTANGLE', 'ARROW'})[wave%5+1]
-        local hp = flr(3+3*(wave^1.2))
+        local hp = flr(3+3*(wave^1.15))
         if type == 'ARROW'  then hp = flr(0.95*hp) end
         if type == 'CIRCLE' then hp = flr(0.95*hp) end
         local gap = type == 'ARROW' and 12 or 10
